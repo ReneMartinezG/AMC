b6f15c268a13a22c9dbc76ffb17f78fc5b2949e9
ReneMartinezG <renemartinezgranados98@gmail.com>
2021-11-01 02:43:55 -0600

    first commit

---
+++ AMC/Controllers/QuestionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMC.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PreguntasAmc()
+        {
+            return View();
+        }
+
+        public ActionResult PreguntasFa()
+        {
+            return View();
+        }
+
+        public ActionResult PreguntasMc()
+        {
+            return View();
+        }
+
+        public ActionResult PreguntasEg()
+        {
+            return View();
+        }
+
+        public ActionResult PreguntasEE()
+        {
+            return View();
+        }
+
+        public ActionResult PreguntasStrib()
+        {
+            return View();
+        }
+
+        public ActionResult Penalidad()
+        {
+            return View();
+        }
+    }
+}
---
+++ AMC/Controllers/ResourcesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMC.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        // GET: Resources
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Mantenimiento()
+        {
+            return View();
+        }
+
+        public ActionResult ProxGanadores()
+        {
+            return View();
+        }
+
+        public ActionResult YaParticipaste()
+        {
+            return View();
+        }
+
+        public ActionResult FinalJuego()
+        {
+            return View();
+        }
+
+        public ActionResult Video()
+        {
+            return View();
+        }
+
+        public ActionResult Explicacion()
+        {
+            return View();
+        }
+    }
+}
